c4721c352c03adda5e7b8e37061a26d6e361c299
毛球 <42747487@qq.com>
2016-10-21 14:03:08 +0800

    del idea

---
+++ app/src/main/java/com/qkl/customview/ActivityManager.java
@@ -0,0 +1,9 @@
+package com.qkl.customview;
+
+/**
+ * 作者：kelingqiu on 16/9/8 15:34
+ * 邮箱：42747487@qq.com
+ */
+
+public class ActivityManager {
+}
---
+++ app/src/main/java/com/qkl/customview/App.java
@@ -0,0 +1,11 @@
+package com.qkl.customview;
+
+import android.app.Application;
+
+/**
+ * 作者：kelingqiu on 16/9/8 15:32
+ * 邮箱：42747487@qq.com
+ */
+
+public class App extends Application {
+}
